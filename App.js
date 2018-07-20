d1bdf86a523f9a4845b49f8f13c7bdc14d1952c1
jorgebaralt <jorgebaraltq@gmail.com>
2018-07-20 16:08:43 -0400

    Added native base for component use, react-navigation to handle screen navigations, lodash. started set up for components and screen.

--- App.js
+++ App.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createBottomTabNavigator,createStackNavigator} from 'react-navigation'
 import {Provider} from 'react-redux';
 import {createStore, compose, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk'
 import reducers from './reducers'
-
+import WelcomeScreen from './screens/WelcomeScreen';
+import AuthScreen from './screens/AuthScreen'
 const store = createStore(reducers,{},compose(applyMiddleware(thunk)));
 
 export default class App extends React.Component {
   render() {
+
+      const MainNavigator = createBottomTabNavigator({
+          welcome:{screen:WelcomeScreen},
+          auth:{screen:AuthScreen}
+          //Auth Screen
+          //Main : Already Logged in Screen
+      });
+
     return (
         <Provider store={store}>
           <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
+              <MainNavigator/>
           </View>
         </Provider>
     );
@@ -22,8 +32,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
 });
---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import {View, Text, AsyncStorage, StyleSheet} from 'react-native';
+import Slides from '../components/Slides';
+import _ from 'lodash';
+import {AppLoading} from'expo'
+
+class WelcomeScreen extends Component{
+    render(){
+        return(
+           <View >
+               <Text> Welcome Screen</Text>
+               <Text> Welcome Screen</Text>
+               <Text> Welcome Screen</Text>
+               <Text> Welcome Screen</Text>
+               <Text> Welcome Screen</Text>
+           </View>
+        )
+    }
+}
+export default WelcomeScreen;
