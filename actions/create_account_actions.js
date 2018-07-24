53ae30b12cfc1e3c5fc152ced265fe7e3005d54b
jorgebaralt <jorgebaraltq@gmaik.com>
2018-07-23 23:50:34 -0400

    Successfully Create Accounts

--- actions/create_account_actions.js
+++ actions/create_account_actions.js
@@ -2,14 +2,27 @@ import {
     CREATE_ACCOUNT_FAIL,
     CREATE_ACCOUNT_SUCCESS
 } from './types'
+import axios from 'axios';
 
 export const createEmailAccount = (user) => async (dispatch) =>{
+    const url = 'https://us-central1-servify-716c6.cloudfunctions.net/createUser'
     const {email,password,firstName,lastName} = user
     //check not empty
     if(email && password && firstName && lastName){
-        //send email and password to back-end to create account.
-        //if successful, dispatch Success
+        //send data to server
+        axios.post(url,{
+            email,
+            password,
+            firstName,
+            lastName
+        }).then((user)=>{
+            console.log("Success account created" + user);
+            dispatch({type:CREATE_ACCOUNT_SUCCESS,payload:user});
+        }).catch((error)=>{
+            console.log('Error axios');
+            dispatch({type:CREATE_ACCOUNT_FAIL,payload:'Email already exist, try a different one'});
+        });
     }else{
-        dispatch({CREATE_ACCOUNT_FAIL})
+        dispatch({type:CREATE_ACCOUNT_FAIL,payload:'Please fill all the information'})
     }
 }
