9246faa7e29ab7383df4389ec7c9e0e681a0f2e1
jorgebaralt <jorgebaraltq@gmaik.com>
2018-08-12 23:58:02 -0400

    working on GET services

---
+++ actions/get_service_actions.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import {
+    GET_SERVICE_SUCCESS,
+    GET_SERTICE_FAIL
+} from './types';
+
+// TODO: grab db_reference of category and subcategory, append to url only 1. 
+// if it has subcategory look for subcategory. else only category
+export const getServicesCategory = () => {
+
+};
+
+export const getServicesSubcategory = () => {
+
+};
--- actions/index.js
+++ actions/index.js
@@ -1,3 +1,4 @@
 export * from './auth_actions';
 export * from './select_category_actions';
 export * from './post_service_actions';
+export * from './get_service_actions';
