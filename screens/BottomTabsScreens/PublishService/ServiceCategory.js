97c2b0d30e4256758d7a9e059bd141dcef2c37e9
jorgebaralt <jorgebaraltq@gmaik.com>
2018-12-27 22:59:53 -0500

    Setup for new Post Service

---
+++ screens/BottomTabsScreens/PublishService/ServiceCategory.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Button } from '../../../components/UI';
+
+const ServiceCategory = (props) => {
+	return (
+		<View>
+			<Text>Content here</Text>
+		</View>
+	);
+};
+
+export default ServiceCategory;
