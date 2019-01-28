706837c572eeb50866be0b5b1f59e3df22c696a1
jorgebaralt <jorgebaraltq@gmaik.com>
2019-01-27 22:11:02 -0500

    fix to delete service

--- screens/ServiceScreens/SpecificServiceScreen.js
+++ screens/ServiceScreens/SpecificServiceScreen.js
@@ -235,7 +235,9 @@ class SpecificServiceScreen extends Component {
 		this.setState({
 			loadingUserComment: false,
 			starCount: 0,
-			dollarCount: 0
+			dollarCount: 0,
+			currentUserReview: null,
+			comment: ''
 		});
 	};
 
