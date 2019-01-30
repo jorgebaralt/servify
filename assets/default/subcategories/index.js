7baa0ff590221392fd97da3dbd7a40bbc106c3ab
jorgebaralt <jorgebaraltq@gmaik.com>
2019-01-30 18:10:20 -0500

    test without images

--- assets/default/subcategories/index.js
+++ assets/default/subcategories/index.js
@@ -1,56 +1,56 @@
 export const getImagePath = (reference) => {
 	switch (reference) {
-		// Home
-		case 'home_cleaning':
-			return require('./home_cleaning.jpg');
-		case 'home_painting':
-			return require('./home_painting.jpg');
-		case 'pool':
-			return require('./pool_services.jpg');
-		case 'carpet':
-			return require('./carpet_clean.jpg');
-		case 'fence':
-			return require('./fence.jpg');
-		case 'other_home':
-			return require('./other_home.jpg');
-		// Auto
-		case 'car_wash':
-			return require('./car_wash.jpg');
-		case 'tire_change':
-			return require('./tire_change.jpg');
-		case 'mechanic':
-			return require('./mechanic.jpg');
-		case 'other_car':
-			return require('./other_car.jpg');
-		// Plumbing
-		case 'water_leak':
-			return require('./water_leak.jpg');
-		case 'toilet':
-			return require('./toilet.jpg');
-		case 'drains':
-			return require('./drains.jpg');
-		case 'other_plumbing':
-			return require('./other_plumbing.jpg');
-		// Handyman
-		case 'ac_repair':
-			return require('./ac_repair.jpg');
-		case 'moving':
-			return require('./moving.jpg');
-		case 'furniture_fix_assembly':
-			return require('./furniture_fix_assembly.jpg');
-		case 'other_handyman':
-			return require('./other_handyman.jpg');
-		// Beauty
-		case 'nails':
-			return require('./nails.jpg');
-		case 'hair':
-			return require('./hair.jpg');
-		case 'spa':
-			return require('./spa.jpg');
-		case 'make_up':
-			return require('./make_up.jpg');
-		case 'other_beauty':
-			return require('./other_beauty.jpg');
+		// // Home
+		// case 'home_cleaning':
+		// 	return require('./home_cleaning.jpg');
+		// case 'home_painting':
+		// 	return require('./home_painting.jpg');
+		// case 'pool':
+		// 	return require('./pool_services.jpg');
+		// case 'carpet':
+		// 	return require('./carpet_clean.jpg');
+		// case 'fence':
+		// 	return require('./fence.jpg');
+		// case 'other_home':
+		// 	return require('./other_home.jpg');
+		// // Auto
+		// case 'car_wash':
+		// 	return require('./car_wash.jpg');
+		// case 'tire_change':
+		// 	return require('./tire_change.jpg');
+		// case 'mechanic':
+		// 	return require('./mechanic.jpg');
+		// case 'other_car':
+		// 	return require('./other_car.jpg');
+		// // Plumbing
+		// case 'water_leak':
+		// 	return require('./water_leak.jpg');
+		// case 'toilet':
+		// 	return require('./toilet.jpg');
+		// case 'drains':
+		// 	return require('./drains.jpg');
+		// case 'other_plumbing':
+		// 	return require('./other_plumbing.jpg');
+		// // Handyman
+		// case 'ac_repair':
+		// 	return require('./ac_repair.jpg');
+		// case 'moving':
+		// 	return require('./moving.jpg');
+		// case 'furniture_fix_assembly':
+		// 	return require('./furniture_fix_assembly.jpg');
+		// case 'other_handyman':
+		// 	return require('./other_handyman.jpg');
+		// // Beauty
+		// case 'nails':
+		// 	return require('./nails.jpg');
+		// case 'hair':
+		// 	return require('./hair.jpg');
+		// case 'spa':
+		// 	return require('./spa.jpg');
+		// case 'make_up':
+		// 	return require('./make_up.jpg');
+		// case 'other_beauty':
+		// 	return require('./other_beauty.jpg');
 		default:
 			return null;
 	}
